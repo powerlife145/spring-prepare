0a749ed240ad4e74d83f760856abe94d36effd0e
uijin <powerlife145@gmail.com>
2023-06-23 15:36:47 +0900

    11

---
+++ controler-prepare/src/test/java/com/example/controlerprepare/ControlerPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlerprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlerPrepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
